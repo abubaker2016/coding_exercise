0dbc4efde3257958643db229a4d42e577c7f78d0
Abu Baker <abubaker.jmi@gmail.com>
2024-10-04 20:31:45 +0530

    first commit

---
+++ backend/Routes/DashboardRouter.js
@@ -0,0 +1,15 @@
+const ensureAuthenticated = require('../Middlewares/Auth');
+
+const router = require('express').Router();
+
+router.get('/', ensureAuthenticated, (req, res) => {
+    console.log('---- logged in user detail ---', req.user);
+    res.status(200).json([
+        {
+           message: "Signup successfully",
+           success: true
+        }
+    ])
+});
+
+module.exports = router;
